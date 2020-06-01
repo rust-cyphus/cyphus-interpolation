87dcd48b38b2078036f85d73b9686adda691ad93
Logan A Morrison <loanmorr@ucsc.com>
2020-05-31 23:17:45 -0700

    Adding module for Givens rotations

---
+++ src/dierckx/fpgivs.rs
@@ -0,0 +1,13 @@
+/// Compute a givens rotation angles given piv and ww.
+#[allow(dead_code)]
+pub(super) fn fpgivs(piv: f64, ww: &mut f64, cos: &mut f64, sin: &mut f64) {
+    let store = piv.abs();
+    let dd = if store >= *ww {
+        store * (1.0 + (*ww / piv).powi(2)).sqrt()
+    } else {
+        *ww * (1.0 + (piv / *ww).powi(2)).sqrt()
+    };
+    *cos = *ww / dd;
+    *sin = piv / dd;
+    *ww = dd;
+}
