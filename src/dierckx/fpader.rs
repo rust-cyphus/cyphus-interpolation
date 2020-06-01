ee3a3d32156c9c509cde989aedffb0aeac77f0a4
Logan A Morrison <loanmorr@ucsc.com>
2020-05-31 23:19:45 -0700

    Adding submodule for computing derivatives

--- src/dierckx/fpader.rs
+++ src/dierckx/fpader.rs
@@ -2,14 +2,14 @@ use ndarray::prelude::*;
 
 /// Calculates the derivatives of a spline of order k1 at the point
 /// t[l] <= x < t[l+1], using the stable recurrence scheme of de Boor
+#[allow(dead_code)]
 pub(super) fn fpader(
-    t: ArrayViewMut1<f64>,
-    n: usize,
-    c: ArrayViewMut1<f64>,
+    t: ArrayView1<f64>,
+    c: ArrayView1<f64>,
     k1: usize,
     x: f64,
     l: usize,
-    d: ArrayViewMut1<f64>,
+    mut d: ArrayViewMut1<f64>,
 ) {
     let mut h = Array1::<f64>::zeros(20);
 
@@ -25,7 +25,7 @@ pub(super) fn fpader(
         let j1 = j + 1;
         if j != 1 {
             let mut i = k1;
-            for jj in j..(k1 + 1) {
+            for _jj in j..(k1 + 1) {
                 let li = i + lk;
                 let lj = li + kj;
                 h[i - 1] = (h[i - 1] - h[i - 1 - 1]) / (t[lj - 1] - t[li - 1]);
@@ -39,7 +39,7 @@ pub(super) fn fpader(
             for jj in j1..(k1 + 1) {
                 let ki = ki - 1;
                 let mut i = k1;
-                for j2 in jj..(k1 + 1) {
+                for _j2 in jj..(k1 + 1) {
                     let li = i + lk;
                     let lj = li + ki;
                     d[i - 1] = ((x - t[li - 1]) * d[i - 1] + (t[lj - 1] - x) * d[i - 1 - 1])
@@ -50,7 +50,7 @@ pub(super) fn fpader(
         }
         d[j - 1] = d[k1 - 1] * fac;
         let ak = k1 - j;
-        fac = fac * ak;
+        fac = fac * ak as f64;
         kj = kj - 1;
     }
 }
