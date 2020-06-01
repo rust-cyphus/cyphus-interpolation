998e50499042b665a85a9238a3075cbef62040d8
Logan A Morrison <loanmorr@ucsc.com>
2020-05-31 23:19:16 -0700

    Adding submodule for computing rotations

---
+++ src/dierckx/fprota.rs
@@ -0,0 +1,9 @@
+/// Apply a Givens rotation to `a` and `b`.
+#[allow(dead_code)]
+#[inline]
+pub(super) fn fprota(cos: f64, sin: f64, a: &mut f64, b: &mut f64) {
+    let stor1 = *a;
+    let stor2 = *b;
+    *b = cos * stor2 + sin * stor1;
+    *a = cos * stor1 - sin * stor2;
+}
